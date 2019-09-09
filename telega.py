d3f26caa2aff4001a332cb009921b282c5e8d32e
FERMA666 <bloq98@gmail.com>
2019-09-09 23:54:44 +0300

    //added ty pifor functional

--- telega.py
+++ telega.py
@@ -1,9 +1,10 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
+import random
 
 
 request_kwargs = {
-    'proxy_url': 'socks5://89.223.92.30:9011',
+    'proxy_url': 'socks5://84.53.239.112:8888',
     'urllib3_proxy_kwargs':{
         'username': 'kek',
         'password': 'kek',
@@ -15,13 +16,26 @@ updater = Updater(token='792596132:AAEwLQ-vPFryBLsLnC7KAFzrI1rvIBMlWZk', use_con
 dispatcher = updater.dispatcher
 
 
-
 def start(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="GOLD ON MY WIRST FUCK THE POLICE")
+    print(update)
+    context.bot.send_message(chat_id=update.message.chat_id, text="DAUN")
 
+
+def pidor(update, context):
+    print(update)
+    ty_pidor = bool(random.getrandbits(1))
+    if ty_pidor:
+        message = "Ты пидор :CC"
+
+    else:
+        message = "Тебе на этот раз повезло"
+
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
 start_handler = CommandHandler('start', start)
+pidor_handler = CommandHandler('pidor', pidor)
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(pidor_handler)
 updater.start_polling()
